6d9bcaff1af19cddecbbdebb5f4b4625a266467c
andrelinhares <doppelzinho@gmail.com>
2024-11-25 16:02:51 +0100

    week6 creating branch and project archives

---
+++ project/scripts/project.js
@@ -0,0 +1,5 @@
+document.querySelector("button").addEventListener("click", function() {
+    const query = document.querySelector("input[type='text']").value;
+    console.log("You searched for:", query);
+    // Add logic to handle the search
+});
